e8c93fe96dd67ede7127602d168b088dd72c824f
Chris Tsongas <chris.tsongas@bitmojo.com>
2012-11-14 00:35:14 -0800

    wrote initial spec tests

--- lib/reorder.rb
+++ lib/reorder.rb
@@ -1,8 +1,20 @@
 class Array
 	def reorder(origin, destination)
-		temp = self[destination]
-		self[destination] = self[origin]
-		self[origin] = temp
-		self
+		if origin < 0 or origin > self.length - 1
+			# origin is before the first or after the last array element
+			raise "Origin is out of range."
+		elsif destination < 0 or destination > self.length - 1
+			# destination is before the first or after the last array element
+			raise "Destination is out of range."
+		elsif destination = origin
+			# origin and destination are the same so do nothing
+			self
+		else
+			# move origin to destination and move other elements accordingly
+			temp = self[destination]
+			self[destination] = self[origin]
+
+			self
+		end
 	end
 end
--- spec/reorder_spec.rb
+++ spec/reorder_spec.rb
@@ -1,8 +1,63 @@
 require 'reorder'
 
 describe "reorder" do
-	it "Should reorder the elements in an array" do
+	it "Should throw an error if the origin is out of range" do
+		array = ['a']
+		expect { array.reorder(1, 0) }.to raise_error(RuntimeError, /Origin is out of range./)
+	end
+
+	it "Should throw an error if the destination is out of range" do
+		array = ['a']
+		expect { array.reorder(0, 1) }.to raise_error(RuntimeError, /Destination is out of range./)
+	end
+
+	it "Should reorder the elements in an array with one element" do
+		array = ['a']
+		array.reorder(0, 0).should == ['a']
+	end
+
+	it "Should reorder the elements in an array with two elements" do
+		array = ['a', 'b']
+		array.reorder(0, 0).should == ['a', 'b']
+	end
+
+	it "Should reorder the elements in an array with two elements" do
+		array = ['a', 'b']
+		array.reorder(1, 0).should == ['b', 'a']
+	end
+
+	it "Should reorder the elements in an array with two elements" do
+		array = ['a', 'b']
+		array.reorder(0, 1).should == ['b', 'a']
+	end
+
+	it "Should reorder the elements in an array with more than two elements" do
+		array = ['a', 'b', 'c']
+		array.reorder(0, 1).should == ['b', 'a', 'c']
+	end
+
+	it "Should reorder the elements in an array with more than two elements" do
+		array = ['a', 'b', 'c']
+		array.reorder(0, 2).should == ['b', 'c', 'a']
+	end
+
+	it "Should reorder the elements in an array with more than two elements" do
 		array = ['a', 'b', 'c']
 		array.reorder(1, 0).should == ['b', 'a', 'c']
+	end
+
+	it "Should reorder the elements in an array with more than two elements" do
+		array = ['a', 'b', 'c']
+		array.reorder(1, 2).should == ['a', 'c', 'b']
+	end
+
+	it "Should reorder the elements in an array with more than two elements" do
+		array = ['a', 'b', 'c']
+		array.reorder(2, 0).should == ['c', 'a', 'b']
+	end
+
+	it "Should reorder the elements in an array with more than two elements" do
+		array = ['a', 'b', 'c']
+		array.reorder(2, 1).should == ['a', 'c', 'b']
 	end
 end
