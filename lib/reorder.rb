471d53471b67fa54c7dce160bafebbced55cfd9f
Chris Tsongas <chris.tsongas@bitmojo.com>
2012-11-25 10:37:41 -0800

    added tests for negative indexes, renamed origin parameter

--- lib/reorder.rb
+++ lib/reorder.rb
@@ -1,18 +1,28 @@
 class Array
-	def reorder(origin, destination)
-		if origin < 0 or origin > self.length - 1
-			# origin is before the first or after the last array element
-			raise "Origin is out of range."
+	def reorder(source, destination)
+
+# Valid indices fall between -n and n-1, where n is the length of the array.
+
+
+
+		# translate any negative indexes to positive values
+
+# a.fetch(4)   #=> IndexError: index 4 outside of array bounds: -2...2
+
+		# validate non-negative values then re-order elements
+		if source < 0 or source > self.length - 1
+			# source is before the first or after the last array element
+			return nil
 		elsif destination < 0 or destination > self.length - 1
 			# destination is before the first or after the last array element
-			raise "Destination is out of range."
-		elsif destination = origin
-			# origin and destination are the same so do nothing
+			return nil
+		elsif destination = source
+			# source and destination are the same so do nothing
 			self
 		else
-			# move origin to destination and move other elements accordingly
+			# move source to destination and move other elements accordingly
 			temp = self[destination]
-			self[destination] = self[origin]
+			self[destination] = self[source]
 
 			self
 		end
--- spec/reorder_spec.rb
+++ spec/reorder_spec.rb
@@ -1,35 +1,71 @@
 require 'reorder'
 
 describe "reorder" do
-	it "Should throw an error if the origin is out of range" do
+	it "Should return nil if the source is out of range" do
 		array = ['a']
-		expect { array.reorder(1, 0) }.to raise_error(RuntimeError, /Origin is out of range./)
+		expect { array.reorder(1, 0) }.should == nil
+	end
+	it "Should return nil if the source is out of range" do
+		array = ['a']
+		expect { array.reorder(-2, 0) }.should == nil
 	end
 
-	it "Should throw an error if the destination is out of range" do
+	it "Should return nil if the destination is out of range" do
 		array = ['a']
-		expect { array.reorder(0, 1) }.to raise_error(RuntimeError, /Destination is out of range./)
+		expect { array.reorder(0, 1) }.should == nil
+	end
+	it "Should return nil if the destination is out of range" do
+		array = ['a']
+		expect { array.reorder(0, -2) }.should == nil
+	end
+
+	it "Should return nil if the source and destination are out of range" do
+		array = ['a']
+		expect { array.reorder(1, -2) }.should == nil
 	end
 
 	it "Should reorder the elements in an array with one element" do
 		array = ['a']
 		array.reorder(0, 0).should == ['a']
 	end
+	it "Should reorder the elements in an array with one element" do
+		array = ['a']
+		array.reorder(-1, -1).should == ['a']
+	end
 
 	it "Should reorder the elements in an array with two elements" do
 		array = ['a', 'b']
 		array.reorder(0, 0).should == ['a', 'b']
 	end
-
 	it "Should reorder the elements in an array with two elements" do
 		array = ['a', 'b']
-		array.reorder(1, 0).should == ['b', 'a']
+		array.reorder(-2, -2).should == ['a', 'b']
 	end
-
+	it "Should reorder the elements in an array with two elements" do
+		array = ['a', 'b']
+		array.reorder(1, 1).should == ['a', 'b']
+	end
+	it "Should reorder the elements in an array with two elements" do
+		array = ['a', 'b']
+		array.reorder(-1, -1).should == ['a', 'b']
+	end
 	it "Should reorder the elements in an array with two elements" do
 		array = ['a', 'b']
 		array.reorder(0, 1).should == ['b', 'a']
 	end
+	it "Should reorder the elements in an array with two elements" do
+		array = ['a', 'b']
+		array.reorder(-2, -1).should == ['b', 'a']
+	end
+	it "Should reorder the elements in an array with two elements" do
+		array = ['a', 'b']
+		array.reorder(1, 0).should == ['b', 'a']
+	end
+	it "Should reorder the elements in an array with two elements" do
+		array = ['a', 'b']
+		array.reorder(-1, -2).should == ['b', 'a']
+	end
+
 
 	it "Should reorder the elements in an array with more than two elements" do
 		array = ['a', 'b', 'c']
