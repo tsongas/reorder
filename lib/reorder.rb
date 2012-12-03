01dadb303746e151ccd4f4870c507a48222de4d8
Chris Tsongas <chris.tsongas@bitmojo.com>
2012-12-03 13:34:14 -0800

    woring on algorithm, adding comments to RSpec tests

--- lib/reorder.rb
+++ lib/reorder.rb
@@ -1,24 +1,86 @@
 class Array
 	def reorder(from, to)
+puts "reorder(#{from}, #{to})"
 		if from < 0 - self.length or from > self.length - 1
 			# from index is out of range (indexes can be negative)
 			nil
 		elsif to < 0 - self.length or to > self.length - 1
 			# to index is out of range (indexes can be negative)
 			nil
-		elsif to = from
+		elsif to == from
 			# from and to are the same so do nothing
 			self
 		else
 			# reorder array
-			(to..from).each do |index|
-				temp = self.index
-				if index = to
-					self.index = self.from
-				else
-					self.index = temp
+			if from < to
+
+puts "temp = self[#{to}]"
+				temp = self[to]
+#puts "temp: #{temp}"
+
+				index = from
+
+#puts "(#{from}..#{target}) do |index|"
+				(from..to).each do
+#puts "index: #{index}"
+
+					if index == from
+						# first iteration
+puts "first self[#{to}] = self[#{from}]"
+						self[to] = self[from]
+
+					elsif index == to
+						# last iteration
+puts "self[#{index - 1}] = temp"
+						self[index - 1] = temp
+
+					else
+						# intermediate iterations
+puts "subsequent self[#{index - 1}] = self[#{index}]"
+						self[index - 1] = self[index]
+
+					end
+
+					index += 1
+#puts "self: #{self}"
+
 				end
+
+			else
+
+puts "temp = self[#{to}]"
+				temp = self[to]
+#puts "temp: #{temp}"
+
+				index = from
+
+#puts "(#{from}..#{target}) do |index|"
+				(from..to).each do
+#puts "index: #{index}"
+
+					if index == from
+						# first iteration
+puts "first self[#{to}] = self[#{from}]"
+						self[to] = self[from]
+
+					elsif index == to
+						# last iteration
+puts "self[#{index - 1}] = temp"
+						self[index - 1] = temp
+
+					else
+						# intermediate iterations
+puts "subsequent self[#{index - 1}] = self[#{index}]"
+						self[index - 1] = self[index]
+
+					end
+
+					index += 1
+#puts "self: #{self}"
+
+
 			end
+			self
 		end
 	end
 end
--- spec/reorder_spec.rb
+++ spec/reorder_spec.rb
@@ -1,6 +1,7 @@
 require 'reorder'
 
 describe "reorder" do
+	# from out of range
 	it "Should return nil if 'from' is out of range" do
 		array = ['a']
 		array.reorder(1, 0).should == nil
@@ -10,6 +11,7 @@ describe "reorder" do
 		array.reorder(-2, 0).should == nil
 	end
 
+	# to out of range
 	it "Should return nil if 'to' is out of range" do
 		array = ['a']
 		array.reorder(0, 1).should == nil
@@ -19,54 +21,57 @@ describe "reorder" do
 		array.reorder(0, -2).should == nil
 	end
 
+	# from and to out of range
 	it "Should return nil if 'from' and 'to' are out of range" do
 		array = ['a']
 		array.reorder(1, -2).should == nil
 	end
 
-	it "Should reorder the elements in an array with one element" do
+	# one array element
+	it "Should reorder(0, 0) the elements in an array with one element" do
 		array = ['a']
 		array.reorder(0, 0).should == ['a']
 	end
-	it "Should reorder the elements in an array with one element" do
+	it "Should reorder(-1, -1) the elements in an array with one element" do
 		array = ['a']
 		array.reorder(-1, -1).should == ['a']
 	end
 
-	it "Should reorder the elements in an array with two elements" do
+	# two array elements
+	it "Should reorder(0, 0) the elements in an array with two elements" do
 		array = ['a', 'b']
 		array.reorder(0, 0).should == ['a', 'b']
 	end
-	it "Should reorder the elements in an array with two elements" do
+	it "Should reorder(-2, -2) the elements in an array with two elements" do
 		array = ['a', 'b']
 		array.reorder(-2, -2).should == ['a', 'b']
 	end
-	it "Should reorder the elements in an array with two elements" do
+	it "Should reorder(1, 1) the elements in an array with two elements" do
 		array = ['a', 'b']
 		array.reorder(1, 1).should == ['a', 'b']
 	end
-	it "Should reorder the elements in an array with two elements" do
+	it "Should reorder(-1, -1) the elements in an array with two elements" do
 		array = ['a', 'b']
 		array.reorder(-1, -1).should == ['a', 'b']
 	end
-	it "Should reorder the elements in an array with two elements" do
+	it "Should reorder(0, 1) the elements in an array with two elements" do
 		array = ['a', 'b']
 		array.reorder(0, 1).should == ['b', 'a']
 	end
-	it "Should reorder the elements in an array with two elements" do
+	it "Should reorder(-2, -1) the elements in an array with two elements" do
 		array = ['a', 'b']
 		array.reorder(-2, -1).should == ['b', 'a']
 	end
-	it "Should reorder the elements in an array with two elements" do
+	it "Should reorder(1, 0) the elements in an array with two elements" do
 		array = ['a', 'b']
 		array.reorder(1, 0).should == ['b', 'a']
 	end
-	it "Should reorder the elements in an array with two elements" do
+	it "Should reorder(-1, -2) the elements in an array with two elements" do
 		array = ['a', 'b']
 		array.reorder(-1, -2).should == ['b', 'a']
 	end
 
-
+	# more than two array elements
 	it "Should reorder the elements in an array with more than two elements" do
 		array = ['a', 'b', 'c']
 		array.reorder(0, 1).should == ['b', 'a', 'c']
