6a33e5adbfa9d7f4c6d0eac5569ba6cd870730f0
Chris Tsongas <chris.tsongas@bitmojo.com>
2012-12-03 15:48:48 -0800

    refactoring, adding comments

--- lib/reorder.rb
+++ lib/reorder.rb
@@ -1,80 +1,80 @@
 class Array
 	def reorder(from, to)
+		# flag for debug output
 		debug = false
+		
+		# if debug show arguments passed to function
 		puts "reorder(#{from}, #{to})" if debug
 
 		if from < 0 - self.length or from > self.length - 1
-			# from index is out of range (indexes can be negative)
+			# 'from' index is out of range (indexes can be negative)
 			nil
 		elsif to < 0 - self.length or to > self.length - 1
-			# to index is out of range (indexes can be negative)
+			# 'to' index is out of range (indexes can be negative)
 			nil
 		elsif to == from
-			# from and to are the same so do nothing
+			# 'from' and 'to' indexes are the same so do nothing, return original array
 			self
 		else
 			# reorder array
 			if from < to
+				# 'from' index is less than 'to' index so loop over increasing values
 
-				puts "temp = self[#{to}]" if debug
+				# save 'to' element for last iteration of loop below
 				temp = self[to]
+				# if debug show statement saving 'to' element 
+				puts "temp = self[#{to}]" if debug
 
-				index = from
-
-				(from..to).each do
-
+				# loop through increasing array indexes
+				(from..to).each_with_index do |index|
 					if index == from
-						# first iteration
-						puts "first self[#{to}] = self[#{from}]" if debug
+						# first iteration move 'from' element to 'to' element
 						self[to] = self[from]
-
+						# if debug show statement moving 'from' element to 'to' element
+						puts "first iteration self[#{to}] = self[#{from}]" if debug
 					elsif index == to
-						# last iteration
-						puts "last self[#{index - 1}] = temp" if debug
+						# last iteration move saved 'to' element next to where it originated
 						self[index - 1] = temp
-
+						# if debug show statement moving saved 'to' element
+						puts "last iteration self[#{index - 1}] = temp" if debug
 					else
-						# intermediate iterations
-						puts "intermediate self[#{index - 1}] = self[#{index}]" if debug
+						# intermediate iterations move index element one element to the left
 						self[index - 1] = self[index]
-
+						# if debug show statement moving index element one element to the left
+						puts "intermediate iteration self[#{index - 1}] = self[#{index}]" if debug
 					end
-
-					index += 1
-
 				end
-
 			else
+				# 'to' index is less than 'from' index so loop over decreasing values
 
-				puts "temp = self[#{to}]" if debug
+				# save 'to' element for last iteration of loop below
 				temp = self[to]
+				# if debug show statement saving 'to' element 
+				puts "temp = self[#{to}]" if debug
 
+				# loop through decreasing array indexes
 				index = from
-
 				from.downto(to) do
-
 					if index == from
-						# first iteration
-						puts "first self[#{to}] = self[#{from}]" if debug
+						# first iteration move 'from' element to 'to' element
 						self[to] = self[from]
-
+						# if debug show statement moving 'from' element to 'to' element
+						puts "first self[#{to}] = self[#{from}]" if debug
 					elsif index == to
-						# last iteration
-						puts "last self[#{index + 1}] = temp" if debug
+						# last iteration move saved 'to' element next to where it originated
 						self[index + 1] = temp
-
+						# if debug show statement moving saved 'to' element
+						puts "last self[#{index + 1}] = temp" if debug
 					else
-						# intermediate iterations
-						puts "intermediate self[#{index + 1}] = self[#{index}]" if debug
+						# intermediate iterations move index element one element to the left
 						self[index + 1] = self[index]
-
+						# if debug show statement moving index element one element to the left
+						puts "intermediate self[#{index + 1}] = self[#{index}]" if debug
 					end
-
 					index -= 1
-
 				end
-
 			end
+			# return reordered array
 			self
 		end
 	end
