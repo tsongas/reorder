f52bd19d583a179b2bf015ba059ff7d372a5c188
Chris Tsongas <chris.tsongas@bitmojo.com>
2012-12-10 16:34:02 -0800

    got cucumber features working, adding cucumber back to .travis.yml

--- features/step_definitions/reorder_steps.rb
+++ features/step_definitions/reorder_steps.rb
@@ -1,3 +1,5 @@
+require File.join(File.dirname(__FILE__), '..', '..', 'lib', 'reorder')
+
 Given /^I have an array with five elements a, b, c, d and e$/ do
   @array = ['a', 'b', 'c', 'd', 'e']
 end
@@ -6,7 +8,7 @@ When /^I move the first element to the position of the third element$/ do
   @array.reorder(0, 2)
 end
 
-Then /^the result returned should be a new array with elements b, c, a, d, e$/ do
+Then /^the result returned should be an array with elements b, c, a, d, e$/ do
   @array.should == ['b', 'c', 'a', 'd', 'e']
 end
 
@@ -14,6 +16,6 @@ When /^I move the fifth element to the position of the third element$/ do
   @array.reorder(4, 2)
 end
 
-Then /^the result returned should be a new array with elements a, b, e, c, d$/ do
+Then /^the result returned should be an array with elements a, b, e, c, d$/ do
   @array.should == ['a', 'b', 'e', 'c', 'd']
 end
